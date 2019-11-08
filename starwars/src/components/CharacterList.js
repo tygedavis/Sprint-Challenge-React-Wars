a31bfb5722b1f74da9d12863d6fff3193c08601c
tygedavis <tyge.davis@gmail.com>
2019-11-08 15:19:08 -0700

    Added buttons that go to the next pages

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -12,30 +12,62 @@ const CharacterListDiv = styled.div`
     width: 600px;
 `;
 
+const Buttons = styled.button`
+    background-color: darkgray;
+    color: white;
+    border: 2px solid black;
+`;
+
+const ButtonDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 10px;
+`;
+
 const CharacterList = () => {
 
     const [character, setCharacter] = useState([]);
     const [number, setNumber] = useState(1)
 
     useEffect(() => {
-    axios.get(`https://lambda-swapi.herokuapp.com/api/people/`)//Grabbing the data
+    axios.get(`https://lambda-swapi.herokuapp.com/api/people/?page=${number}`)//Grabbing the data
         .then(res => {
-            console.log("Console logging the data from axios: ", res.data.results)//Gives back information on Luke Skywalker
+            console.log("Console logging the data from axios: ", res.data)//Gives back information on Luke Skywalker
             setCharacter(res.data.results)//Sets character to the information on Luke Skywalker
         })
         .catch(err => {
             console.log('There was an error in the Axios call: ', err)
         })
-    }, [])
+    }, [number])
+
+    function buttonUp(){
+        setNumber(number + 1);
+        //console.log(number)
+    }
+
+    function buttonDown(){
+        if(number > 1){
+            setNumber(number - 1);
+        }
+    }
 
     //console.log("This is character: ", character);
 
     return(
-        <CharacterListDiv>
-            {character.map((person, index) => { //for every character, we want to make a new card. So we made character.map
-                return <CharacterCard key={index} name={person.name} birth={person.birth_year}/>   //We just wanted the information of name and birth. Passing these down in props to CharacterCard
-            })}
-        </CharacterListDiv>
+        <>
+            <CharacterListDiv>
+                {character.map((person, index) => { //for every character, we want to make a new card. So we made character.map
+                    return <CharacterCard key={index} name={person.name} birth={person.birth_year}/>   //We just wanted the information of name and birth. Passing these down in props to CharacterCard
+                })}
+            <ButtonDiv>
+                <Buttons onClick={buttonDown}>Previous Page</Buttons>
+                <Buttons onClick={buttonUp} >Next Page</Buttons>
+            </ButtonDiv>
+            </CharacterListDiv>
+
+        </>
         );
 }
 
